91c8280b3ab11e020b0df72da73e050d5fbf043f
Greg Rotman <Greg.Rotman@inontime.com>
2021-01-26 14:44:50 -0500

    Add project files.

---
+++ PRS/Engine/GameEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRS.Engine
+{
+    public class GameEngine
+    {
+        private int playerCount;
+        public GameEngine(int players)
+        {
+            playerCount = players;
+        }
+
+        public string Throw(string Choice)
+        {
+            return "";
+        }
+
+        public void AddPlayer()
+        {
+            playerCount = playerCount + 1;
+        }
+
+        public int GetPlayerCount()
+        {
+            return playerCount; 
+        }
+
+        private int CalculateComputerChoice()
+        {
+            return new Random().Next(0, 3);
+        }
+    }
+}
---
+++ PRS/Program.cs
@@ -0,0 +1,25 @@
+﻿using PRS.Engine;
+using System;
+
+namespace PRS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+            GameEngine myEngine = new GameEngine(2);
+            Console.WriteLine($"My current player count is {myEngine.GetPlayerCount()}");
+            myEngine.AddPlayer();
+            Console.WriteLine($"My current player count is {myEngine.GetPlayerCount()}");
+
+
+            var exitText = "";
+            while (string.Equals(exitText, "E", StringComparison.OrdinalIgnoreCase) == false)
+            {
+                Console.WriteLine("Type 'E' to exit");
+                exitText = Console.ReadLine();
+            }
+        }
+    }
+}
